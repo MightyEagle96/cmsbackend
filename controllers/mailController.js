9b4216fe5a04f1a6cf67ed9751c77d36deab8a0e
Dubem Ikechukwu <mightyeaglecorp@gmail.com>
2024-06-21 11:00:41 +0100

    made changes

--- controllers/mailController.js
+++ controllers/mailController.js
@@ -18,8 +18,8 @@ export const outgoneMails = async (req, res) => {
 
 const emailTemplate = (name, content, outro) => {
   const mailGenerator = new MailGen({
-    theme: "default",
-    product: { name: "JAMB ITS", link: "https://jamb.gov.ng" },
+    theme: "salted",
+    product: { name: "JAMB FINANCE & ACCOUNTS", link: "https://jamb.gov.ng" },
   });
 
   const email = {
@@ -27,7 +27,7 @@ const emailTemplate = (name, content, outro) => {
       name: name,
       intro: content,
       outro,
-      greeting: "Good Greetings",
+      greeting: "Good Greetings, ",
       signature: "Thanks",
     },
   };
@@ -48,16 +48,25 @@ const transporter = nodemailer.createTransport({
     pass: process.env.SENDER_PASSWORD,
   },
 });
-async function main(emails, body) {
-  const info = transporter.sendMail({
-    from: { name: "JAMB ITS", address: process.env.SENDER_USER },
-    to: emails,
-    subject: body.subject,
-    text: body.content,
-    // html: ,
-    html: emailTemplate(body.name, body.content),
-  });
-  info.then(() => console.log("Message sent")).catch("Could not send message");
+async function main(emails, body, index) {
+  try {
+    const info = transporter.sendMail({
+      from: {
+        name: "JAMB FINANCE & ACCOUNTS DEPARTMENTS",
+        address: process.env.SENDER_USER,
+      },
+      to: emails,
+      subject: body.subject,
+      text: body.content,
+      // html: ,
+      html: emailTemplate(body.name, body.content),
+    });
+    info
+      .then(() => console.log(`Mail sent to ${emails}. -${index}`))
+      .catch(`Could not send mail sent to ${emails}`);
+  } catch (error) {
+    console.log(new Error(error).message);
+  }
 }
 
 const centreVariable = "{centre}";
@@ -87,6 +96,58 @@ export const sendMailPro = (req, res) => {
     }
 
     res.send("Mail sent");
+  } catch (error) {
+    res.status(500).send(new Error(error).message);
+  }
+};
+
+export const sendMailVersion2 = (req, res) => {
+  try {
+    const recipients = req.body;
+    for (let i = 80; i < 100; i++) {
+      const recipient = recipients[i];
+
+      const content = `This is to notify you that the sum of ${recipients[i].amountPaid} Naira has been paid to ${recipients[i].centrename}. \nAccount Number: ${recipients[i].accountNumber} ${recipients[i].bankName} Bank. \nRemitta Ref Number: ${recipients[i].remittaRefNumber}. \nDate Paid: ${recipients[i].datePaid}.`;
+
+      const mailBody = {
+        subject: `2024 UTME CBT CENTRE SERVICE CHARGE PAYMENT`,
+        content,
+        name: recipients[i].Name,
+        //name: recipient.adminName.toUpperCase() || "",
+      };
+      if (recipient.email)
+        main(recipient.email, mailBody, i + 1).catch(console.error);
+    }
+
+    res.send("Mail sent");
+  } catch (error) {
+    res.status(500).send(new Error(error).message);
+  }
+};
+
+export const sendMailToSupervisors = (req, res) => {
+  try {
+    const recipients = req.body;
+
+    for (let i = 0; i < recipients.length; i++) {
+      const recipient = recipients[i];
+
+      const content = `This is to notify you that you are yet to refund the sum of N${recipients[
+        i
+      ].amountPaid.toLocaleString()} that was paid to you for the 2024 UTME exercise, for being absent for more than 50% of your examination days. Kindly refund the money and send a mail to shallyboy1@gmail.com with an attachment of proof of payment.`;
+
+      const mailBody = {
+        subject: `NOTICE TO REFUND`,
+        content,
+        name: recipients[i].name.toUpperCase(),
+        //name: recipient.adminName.toUpperCase() || "",
+      };
+      if (recipient.email && recipient.email !== "NULL")
+        //console.log(mailBody);
+        main(recipient.email, mailBody, i + 1).catch(console.error);
+    }
+
+    res.send("Message sent");
   } catch (error) {
     res.status(500).send(new Error(error).message);
   }
--- routers/mailRouter.js
+++ routers/mailRouter.js
@@ -1,9 +1,16 @@
 import express from "express";
 
-import { sendMailPro } from "../controllers/mailController.js";
+import {
+  sendMailPro,
+  sendMailToSupervisors,
+  sendMailVersion2,
+} from "../controllers/mailController.js";
 const mailRouter = express.Router();
 
 //center
-mailRouter.post("/send", sendMailPro);
+mailRouter
+  .post("/send", sendMailPro)
+  .post("/sendmail", sendMailVersion2)
+  .post("/sendmailtosupervisors", sendMailToSupervisors);
 
 export default mailRouter;
